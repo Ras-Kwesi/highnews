0c1a1e55d77392a6143003ec56c47c55431683ca
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-08-31 17:00:34 +0300

    views for categories added

--- app/main/views.py
+++ app/main/views.py
@@ -10,6 +10,12 @@ def index():
     The function for our index html for what will be displayed
     '''
 
-    title = "Welcome to Newsance"
+    general_news = request_news('general')
+    sports_news = request_news('sports')
+    business_news = request_news('business')
+    entertainment_news = request_news('entertainment')
+    science_news = request_news('science')
+    tech_news = request_news('technology')
+    title = "Newsance"
 
-    return render_template('index.html',title=title)
+    return render_template('index.html',title=title, general = general_news, sports = sports_news, science = science_news, business = business_news, tech = tech_news, enter = entertainment_news)
