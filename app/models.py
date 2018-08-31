04471002f76818756bc897889ca251a0b849763d
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-08-31 10:54:38 +0300

    Class models defined for articles and sources

--- app/models.py
+++ app/models.py
@@ -1,10 +1,15 @@
-class News :
+class Articles :
     '''
     definition of the news class properties
     '''
 
-    def __init__(self,):
-
+    def __init__(self,title,author,description,url,urlToImage,publishedAt):
+        self.title = title
+        self.author = author
+        self.description = description
+        self.url = url
+        self.image = urlToImage # The link to the URL will be object.image, which points to urlToImage
+        self.time = publishedAt
 
 
 
@@ -18,6 +23,12 @@ class Source :
 
     sourceNewsList = []
 
-    def __init__(self):
+    def __init__(self, id, name, url, category, language, country):
+        self.id = id
+        self.name = name
+        self.url = url
+        self.category = category
+        self.language = language
+        self.country = country
 
 
