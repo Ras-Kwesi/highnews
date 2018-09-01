9a4eff2fffddbf795777e12c7c0a9a997cab2d8e
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-09-01 12:25:20 +0300

    sporces diplayed

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     NEWS_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
-    API_KEY = os.environ.get('51664d7eabf94215b7a3a91eab4f0cad')
+    API_KEY = os.environ.get('API_KEY')
 
 
 
